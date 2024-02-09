53aa2172926312b144c6785a988e28a3a9a7af9b
myNameIsIon <iona8ansideras@gmail.com>
2024-02-09 19:55:33 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   // Set the base URL for the project
-   base: "/virtualAssistant/"
 })
