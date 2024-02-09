7611e4aa4ab88b6a6168a5fdc4593d353f0dc801
myNameIsIon <iona8ansideras@gmail.com>
2024-02-09 19:41:19 +0200

    Update package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    // Set the base URL for the project
-   base: "/truckMate/"
+   base: "/virtualAssistant/"
 })
